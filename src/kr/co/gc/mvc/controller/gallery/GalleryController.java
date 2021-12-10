118714c902ebd5dbeb25cc34c87deae447ba3ed2
Gunter <ramses203@naver.com>
2021-12-10 23:49:13 +0900

    2021-12-10
    2021-12-10

--- src/kr/co/gc/mvc/controller/gallery/GalleryController.java
+++ src/kr/co/gc/mvc/controller/gallery/GalleryController.java
@@ -1,21 +1,75 @@
 package kr.co.gc.mvc.controller.gallery;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.gc.mvc.dao.GalleryDaoInter;
+import kr.co.gc.mvc.dto.GalleryDto;
 
 @Controller
 public class GalleryController {
-
+	@Autowired
+	private GalleryDaoInter galleryDaoInter; 
+	
+	
+	
+	
 	@RequestMapping("/galleryList") 
-	public String galleryList() {
+	public String galleryList(Model m) {
+		List<GalleryDto> list = galleryDaoInter.getlist();
+		m.addAttribute("list",list);
 		return "gallery/gallerylist";
 	}
 	
-	@RequestMapping("/galleryInsert")
-	public String galleryInsert() {
-		System.out.println("galleryInsertµø¿€«‘");
-		return "redirect:galleryList";
+	@RequestMapping("/galleryInsertForm")
+	public String galleryInsertForm() {
+		return "gallery/galleryInsertForm";
 	}
 	
+	@PostMapping("/galleryInsert")
+	public String galleryInsert(GalleryDto dto) {
+		galleryDaoInter.addUP(dto);
+		return "gallery/gallerylist";	
+	}
+	
+	@GetMapping("/gallerydetail")
+	public String updetailList(Model m, int gno) {
+		GalleryDto dto = galleryDaoInter.getDetail(gno);
+		m.addAttribute("dto", dto);
+		return "gallery/gallerydetail";
+	}
+	
+	@GetMapping("/galleryUpdateForm")
+	public String updateForm(Model m, int gno) {
+		GalleryDto dto = galleryDaoInter.getDetail(gno);
+		m.addAttribute("dto", dto);
+		return "gallery/galleryUpdateForm";
+	}
+	
+	@PostMapping("/galleryUpdate")
+	public String updateLogic(GalleryDto dto) {
+		galleryDaoInter.update(dto);
+		return "redirect:/galleryList";
+	}
+	
+	@GetMapping("/galleryDelete")
+	public String deleteLogic(GalleryDto dto) {
+		galleryDaoInter.delete(dto);
+		return "redirect:/galleryList";
+	}
+	
+	@GetMapping("/galleryLike")
+	public String updateLike(GalleryDto dto) {
+		galleryDaoInter.likeUpdate(dto);
+		return "redirect:/galleryList";
+	}
 }
 
